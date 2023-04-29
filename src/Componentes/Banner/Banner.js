11750045a6661c50f6aa91157ac5a9509e4191aa
Matheus Quevedo <matheusq10@gmail.com>
2023-04-29 02:30:35 -0300

    Criação do banner

---
+++ src/Componentes/Banner/Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css'
+
+function Banner() {
+    // JSX
+    return (
+        <header className="banner">
+            <img src="/img/banner.png" alt="Banner principal da página Organo" />
+        </header>
+    )
+}
+
+export default Banner
