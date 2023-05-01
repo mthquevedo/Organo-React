032cbbcb120a08db253c05a4be1d57a1bc649747
Matheus Quevedo <matheusq10@gmail.com>
2023-05-01 13:05:24 -0300

    Configuração de submit

--- src/Componentes/Formulario/index.js
+++ src/Componentes/Formulario/index.js
@@ -1,3 +1,4 @@
+import Botao from "../Botao";
 import CampoTexto from "../CampoTexto";
 import ListaSuspensa from "../ListaSuspensa";
 import "./Formulario.css";
@@ -13,14 +14,19 @@ const Formulario = () => {
     "Inovação e Gestão",
   ];
 
+  const aoSalvar = (evento) => {
+    evento.preventDefault()
+  }
+
   return (
     <section className="formulario">
-      <form>
+      <form onSubmit={aoSalvar}>
         <h2>Prencha os dados para criar o card do colaborador</h2>
-        <CampoTexto label="Nome" placeholder="Digite seu nome" />
-        <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
+        <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome" />
+        <CampoTexto obrigatorio={true}  label="Cargo" placeholder="Digite seu cargo" />
         <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
-        <ListaSuspensa label="Time" itens={times} />
+        <ListaSuspensa obrigatorio={true} label="Time" itens={times} />
+        <Botao>Criar Card</Botao>
       </form>
     </section>
   );
